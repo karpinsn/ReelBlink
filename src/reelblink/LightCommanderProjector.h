600aa3f434cbe61646fd3b2bddd60302fd0f8a6c
Nik Karpinsky <karpinsn@iastate.edu>
2013-04-02 10:33:55 -0500

    Added in batch commands to startup and initialize the projector

--- src/reelblink/LightCommanderProjector.h
+++ src/reelblink/LightCommanderProjector.h
@@ -1,9 +1,6 @@
 #ifndef PROJECTOR_LIGHT_COMMANDER_H_
 #define PROJECTOR_LIGHT_COMMANDER_H_
 
-#define MSB_HIGH 0x10000000
-#define MSB_LOW 0x01111111
-
 #include <algorithm>
 #include <memory>
 #include <PortabilityLayer.h>
@@ -14,6 +11,10 @@ using namespace std;
 
 class LightCommanderProjector : public IProjector
 {
+private:
+  static const Byte MSB_HIGH  = 0x80;
+  static const Byte MSB_LOW	  = 0x7F;
+
 public:
   LightCommanderProjector(void);
   ~LightCommanderProjector(void);
@@ -26,8 +27,21 @@ public:
 private:
   bool _SetLED(LED_t led, float intensity);
   unique_ptr<Byte[]> _Convert2BinaryImage(cv::Mat byteImage);
+  
+ /**
+  * Used to write registers to start the light commander projecting.
+  * This is undocumented and taken from sniffing their communication,
+  * so it is subject to change. Using this you dont have to run their software
+  */
+  bool _WritePlayRegister( void );
   bool _CheckLogError(Status status);
 };
+
+#define DLP_CHECKRETURNBATCHCOMMAND(COMMAND) \
+{ \
+  if( !_CheckLogError( RunBatchCommand(COMMAND) ) )\
+	{ return false; } \
+}
 
 void LightCommanderCCallback(String loggingCallback);
 
